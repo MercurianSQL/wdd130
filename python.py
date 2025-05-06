19a306e71b5b13b8710b1743a488e320a6f4fdf9
MercurianSQL <sistermarinda@gmail.com>
2025-05-06 16:05:29 -0600

    trusted

---
+++ python.py
@@ -0,0 +1,24 @@
+name = "bob"
+age = 24
+
+print ("Hello {name} you are {age} years old.")
+
+place = "Rexburg"
+print(f"You are from {place}.")
+
+place = input ("Where are you from? ")
+print(f"You are from {place}.")
+
+vacarion_place = input ("Where do you want to go on vacation?" )
+
+color = input ("Please type your favorite color: ")
+print ("Your favorite color is")
+print (color)
+#  why does this use parenthesis instead of curly brackets?
+name = input ("Pleae enter your name: ")
+print (name)
+print ()
+print ("Did you see that blank line?")
+print ("Blank line \n in the middle of string")
+
+
