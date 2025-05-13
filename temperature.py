ae645468c371b94d97b64d7f828e49be3055e6d3
MercurianSQL <sistermarinda@gmail.com>
2025-05-13 15:58:51 -0600

    spacing

--- temperature.py
+++ temperature.py
@@ -1,8 +1,8 @@
 #To convert degrees in Fahrenheit to Celsius you need to subtract 32
 # from the Fahrenheit amount and then multiply it by the fraction 5/9
 
- print(F_degree=input("What is the temperature in Fahrenheit? "))
- C_degree=((F_degree - 32) * (5/9))
+print(F_degree=input("What is the temperature in Fahrenheit? "))
+C_degree=((F_degree - 32) * (5/9))
 
 print(f"The temperature in Celsius is {C_degree} degrees.")
 
